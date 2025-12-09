b2259f1baa97ba86d687a796a08105360020e329
pham-ng <phamnguyenkhanhminh1502@gmail.com>
2025-12-09 12:34:15 +0700

    folder structure

---
+++ template.sh
@@ -0,0 +1,17 @@
+#create directory
+
+mkdir -p src
+mkdir -p research
+
+# create files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch README.md
+touch requirements.txt
+touch research/trials.ipynb
+
+echo "Project structure created successfully."
